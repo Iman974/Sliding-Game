45df1f3168c8cc44fded8edf110dbd60409a63cc
Iman Toumi <reyan.toumi@gmail.com>
2018-04-29 22:06:59 +0400

    Finished core gameplay

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,62 +1,131 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-    private enum SlideDirection {
+    [Header("Delays")]
+    [SerializeField] private float skipDelay = 1.5f;
+    [SerializeField] private float nextDelay = 0.5f;
+    [Header("")]
+    [SerializeField] private int baseScoreValue = 10;
+    [Tooltip("The threshold that need to be reached in order to consider the sliding. This is a value squared.")]
+    [SerializeField] private float sensibility = 0.07f;
+
+    private float countdown;
+    private float inverseSkipDelay; // Used to remap values to 0 to 1
+    private SlideDirection currentDirection = SlideDirection.Right;
+    private int totalScore;
+    private bool isReady = true;
+
+    private event System.Action<int> _ValidationEvent;
+    private event System.Action<SlideDirection> _NextEvent;
+
+    public enum SlideDirection {
         Left,
         Right,
         Up,
         Down
     }
 
-    [SerializeField] private float skipDelay = 1.5f;
+    public static GameManager Instance { get; private set; }
 
-    private float countdown;
-    private SlideDirection currentDirection;
+    public static event System.Action<int> ValidationEvent {
+        add {
+            Instance._ValidationEvent += value;
+        }
+        remove {
+            Instance._ValidationEvent -= value;
+        }
+    }
+    public static event System.Action<SlideDirection> NextEvent {
+        add {
+            Instance._NextEvent += value;
+        }
+        remove {
+            Instance._NextEvent -= value;
+        }
+    }
 
-    private void Start() {
+    private void Awake() {
+        #region Singleton
+        if (Instance == null) {
+            Instance = this;
+        } else if (Instance != this) {
+            Destroy(this);
+            return;
+        }
+        #endregion
+
         countdown = skipDelay;
+        inverseSkipDelay = 1f / skipDelay;
     }
 
     private void Update() {
+        if (!isReady) {
+            return;
+        }
+
         countdown -= Time.deltaTime;
 
         if (countdown <= 0f) {
             countdown = skipDelay;
             // -1 life
+            Debug.Log("Missed !");
 
             Next();
-        } else if (Input.touchCount > 0) {
+        } else if (Input.touchCount > 0 && isReady) {
             Touch touch = Input.GetTouch(0);
 
-            if (touch.phase == TouchPhase.Moved) {
-                SlideDirection dir;
+            if (touch.phase == TouchPhase.Moved && touch.deltaPosition.sqrMagnitude > sensibility) {
+                Debug.Log("Slide: " + touch.deltaPosition);
+                countdown = skipDelay;
+                isReady = false;
+                SlideDirection inputDirection;
 
-                if (touch.deltaPosition.x > 0f) {
-                    dir = SlideDirection.Right;
-                } else if (touch.deltaPosition.x < 0f) {
-                    dir = SlideDirection.Left;
-                } else if (touch.deltaPosition.y > 0f) {
-                    dir = SlideDirection.Up;
+                if (Mathf.Abs(touch.deltaPosition.x) > Mathf.Abs(touch.deltaPosition.y)) {
+                    inputDirection = Mathf.Sign(touch.deltaPosition.x) == 1f ? SlideDirection.Right : SlideDirection.Left;
                 } else {
-                    dir = SlideDirection.Down;
+                    inputDirection = Mathf.Sign(touch.deltaPosition.y) == 1f ? SlideDirection.Up : SlideDirection.Down;
                 }
 
-                ValidateMovement(dir);
-                Next();
+                Debug.Log("Input: " + currentDirection + ", " + touch.deltaPosition);
+                ValidateMovement(inputDirection);
+                StartCoroutine(TriggerNextDelayed());
             }
         }
     }
 
-    private void ValidateMovement(SlideDirection direction) {
-        if (direction == currentDirection) {
-            // Give points
+    private IEnumerator TriggerNextDelayed() {
+        yield return new WaitForSeconds(nextDelay);
+        Next();
+        isReady = true;
+    }
+
+    private void ValidateMovement(SlideDirection inputDirection) {
+        if (inputDirection == currentDirection) {
+            Debug.Log("Win: " + inputDirection);
+            totalScore += CalculateScore();
         } else {
-            // Wrong ! -1 life ?
+            Debug.Log("Lose: " + inputDirection);
+            totalScore -= baseScoreValue;
         }
+
+        if (_ValidationEvent != null) {
+            _ValidationEvent(totalScore);
+        }
+    }
+
+    private int CalculateScore() {
+        float timeElapsed = countdown * inverseSkipDelay;
+
+        return Mathf.RoundToInt(baseScoreValue * timeElapsed);
     }
 
     private void Next() {
         currentDirection = (SlideDirection)Random.Range(0, 4);
+
+        if (_NextEvent != null) {
+            _NextEvent(currentDirection);
+        }
     }
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,16 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField] private Text scoreText;
+    [SerializeField] private Image directionImg;
+
+    public void OnMovementValidation(int scoreValue) {
+        scoreText.text = "Score: " + scoreValue;
+    }
+
+    private void Start() {
+        GameManager.ValidationEvent += OnMovementValidation;
+        GameManager.NextEvent += OnNext;
+    }
+
+    public void OnNext(GameManager.SlideDirection direction) {
+        float rotation = 0;
+
+        if (direction == GameManager.SlideDirection.Right) {
+            rotation = 90f;
+        } else if (direction == GameManager.SlideDirection.Down) {
+            rotation = 180f;
+        } else if (direction == GameManager.SlideDirection.Left) {
+            rotation = 270f;
+        }
+
+        directionImg.transform.rotation = Quaternion.Euler(0f, 0f, 90f - rotation);
+    }
+
+    private void OnDestroy() {
+        GameManager.ValidationEvent -= OnMovementValidation;
+        GameManager.NextEvent -= OnNext;
+    }
 }
